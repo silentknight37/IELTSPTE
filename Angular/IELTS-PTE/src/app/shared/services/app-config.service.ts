cca4d75c1a45aa0ba4e6d3c7825ddba5eafcc437
Shanaka Kariyawasam <shanaka.kariyawasam@gmail.com>
2020-08-27 21:10:24 +0530

    push
    push

--- Angular/IELTS-PTE/src/app/shared/services/app-config.service.ts
+++ Angular/IELTS-PTE/src/app/shared/services/app-config.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppConfigService {
-//url:any="https://host.pte-ielts.com/";
 url:any="https://localhost:5001/";
 constructor() { }
 
